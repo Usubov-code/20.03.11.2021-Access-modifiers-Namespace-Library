cd06e4024037b471740db7ee11bcb3ccd983b277
Usubov-code <tu201806104@code.edu.az>
2021-11-03 18:26:52 +0400

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        
+        public static int Meth1(int a,int b)
+        {
+            Console.Write("toplamanin cavabi  ");
+            return a + b;
+
+
+        }
+        public static int Meth2(int a,int b)
+        {
+            Console.Write(" cixmanin cavab  ");
+
+            return a - b;
+
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp3
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the 1nd number") ;
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the 2nd number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Class1.Meth1(a, b));
+            Console.WriteLine(Class1.Meth2(a, b));
+           
+
+
+        }
+    }
+}
